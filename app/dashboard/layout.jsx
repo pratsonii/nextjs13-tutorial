ff9f4f51b4d67e873c76960424198901d449f779
Pratik Soni <100229748+PratikLaweb@users.noreply.github.com>
2023-11-16 17:57:52 -0500

    Navigation bar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import styles from "../ui/dashboard/dashboard.module.css"
+import Navbar from "../ui/dashboard/navbar/navbar"
 
 const Layout = ({children}) => {
   return (
@@ -8,6 +9,7 @@ const Layout = ({children}) => {
         <Sidebar/>
       </div>
       <div className={styles.content}>
+        <Navbar/>
         {children}
       </div>
     </div>
