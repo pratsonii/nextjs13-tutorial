dc0398dc718a6e0e852a4fc9cf98721fd6068790
Pratik Soni <100229748+PratikLaweb@users.noreply.github.com>
2023-11-18 12:16:27 -0500

    Cards feature

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,18 @@
+import { cards } from "../lib/data";
+import Card from "../ui/dashboard/card/card";
 import styles from "../ui/dashboard/dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <div></div>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {cards.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
