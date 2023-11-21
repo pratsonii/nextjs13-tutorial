3fa38b025c78f6601be181535025a68c0e253e3c
Pratik Soni <100229748+PratikLaweb@users.noreply.github.com>
2023-11-21 17:44:22 -0500

    Transaction table

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,7 @@
 import { cards } from "../lib/data";
 import Card from "../ui/dashboard/card/card";
 import styles from "../ui/dashboard/dashboard.module.css";
+import Transactions from "../ui/dashboard/transactions/transactions";
 
 const Dashboard = () => {
   return (
@@ -11,6 +12,7 @@ const Dashboard = () => {
             <Card item={item} key={item.id} />
           ))}
         </div>
+        <Transactions />
       </div>
     </div>
   );
